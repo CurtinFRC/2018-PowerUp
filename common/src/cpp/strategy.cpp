685b8a757b2f02ba790566a0e5a81b172dff633a
Mark Swan <mark.swan@outlook.com.au>
2018-01-24 14:24:43 +0800

    Add Strategy system

---
+++ common/src/cpp/strategy.cpp
@@ -0,0 +1,39 @@
+#include "curtinfrc/strategy/strategy.h"
+
+#include <chrono>
+
+using namespace curtinfrc;
+
+void StrategyController::periodic() {
+  if (active != nullptr) {
+    auto now = std::chrono::system_clock::now();
+    double now_ms = static_cast<double>(std::chrono::time_point_cast<std::chrono::milliseconds>(now).time_since_epoch().count());
+    if (active->first_run) {
+      active->first_run = false;
+      active->start_time = now_ms;
+      active->start();
+    }
+    active->tick(now_ms - active->start_time);
+    if (active->done || now_ms - active->start_time > active->timeout) {
+      active->stop();
+      active = active->_next;
+    }
+  }
+}
+
+void StrategyController::set_active(std::shared_ptr<Strategy> strat) {
+  active = strat;
+}
+
+void StrategyController::append(std::shared_ptr<Strategy> strat) {
+  std::shared_ptr<Strategy> current = active;
+  bool crawl = true;
+  while (crawl) {
+    if (current->_next != nullptr) {
+      current = current->_next;
+    } else {
+      current->_next = strat;
+      crawl = false;
+    }
+  }
+}
---
+++ common/src/include/curtinfrc/strategy/strategy.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <memory>
+
+namespace curtinfrc {
+
+  /**
+   * A 'Strategy' is a single component in a sequential chain of events or actions
+   * it is a class wrapper for a periodic-based function run in a sequential manner
+   */
+  class Strategy {
+  public:
+    /**
+     * Called upon the Strategy being run for the first time per instance
+     */
+    virtual void start() {}
+
+    /**
+     * Called at the same rate as the robot base class
+     */
+    virtual void tick(double time) = 0;
+
+    /**
+     * Called upon the Strategy being 'done'
+     */
+    virtual void stop() {}
+    /**
+     * Set the strategy to execute after this one. Permitted to be nullptr
+     */
+    void then(std::shared_ptr<Strategy> next) { _next = next; }
+
+    void set_timeout(double seconds) { timeout = seconds*1000; }
+
+    bool done = false, first_run = true;
+    double start_time = 0, timeout = -1;
+    std::shared_ptr<Strategy> _next = nullptr;
+  };
+
+  /**
+   * The StrategyController is the conductor for the Strategy system. It keeps
+   * track of the currently running strategy and coordinates when to switch to
+   * the next one.
+   */
+  class StrategyController {
+  public:
+    /**
+     * Called periodically at the same rate (or higher) than the main robot class,
+     * in any state.
+     */
+    void periodic();
+
+    /**
+     * Set the currently active Strategy. If there is already a Strategy in place,
+     * this will kick out the previous Strategy and replace it.
+     */
+    void set_active(std::shared_ptr<Strategy> strat);
+
+    /**
+     * Add a Strategy onto the end of the Strategy chain. This will execute after all
+     * currently queued strategies are completed.
+     */
+    void append(std::shared_ptr<Strategy> strat);
+
+  private:
+    std::shared_ptr<Strategy> active;
+  };
+} // namespace curtinfrc
