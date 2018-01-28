9c7272bc45022cbd3880d2de5576e2615d2ad90f
Unknown <alistairp12@gmail.com>
2018-01-28 19:17:56 +0800

    Update vision to a working state

--- common/vision/src/capture.cpp
+++ common/vision/src/capture.cpp
@@ -3,8 +3,7 @@
 void capture::init()
 {
 	video_capture = new cv::VideoCapture(0);
-	if (!video_capture->isOpened()) 
-	{
+	if (!video_capture->isOpened()) {
 		std::cerr << "Could not open camera." << std::endl;
 		exit(-1);
 	}
@@ -19,11 +18,27 @@ void capture::run()
 	frame_mtx.lock();
 	(*video_capture) >> frame;
 	frame_mtx.unlock();
+	cap_cv.notify_all();
 	cv::waitKey(10);
 }
 
 cv::Mat* capture::get_frame()
 {
-	std::lock_guard<std::mutex> lock_guard(frame_mtx);
 	return &frame;
+}
+
+void capture::lock() {
+	frame_mtx.lock();
+}
+
+void capture::unlock() {
+	frame_mtx.unlock();
+}
+
+std::shared_ptr<std::unique_lock<std::mutex> > capture::cv_wait()
+{
+	auto u_lock = std::make_shared<std::unique_lock<std::mutex> >(frame_mtx);
+	cap_cv.wait(*u_lock);
+
+	return u_lock;
 }
--- common/vision/src/processing.h
+++ common/vision/src/processing.h
@@ -2,9 +2,10 @@
 #include <opencv2/opencv.hpp>
 #include <mutex>
 #include <vector>
+#include <algorithm>
+#include <condition_variable>
 
-class processing 
-{
+class processing {
 	public:
 		void process_frame(cv::Mat* frame);
 		cv::Mat* get_thresholded();
@@ -12,10 +13,16 @@ class processing
 		std::vector< std::vector<cv::Point> >* get_bounding_boxes();
 		std::vector<int>* get_centre_xs();
 
+		std::unique_lock<std::mutex>* cv_wait();
+
 	private:
 		cv::Mat thresholded;
 		std::vector< std::vector<cv::Point> > filtered_contours;
 		std::vector< std::vector<cv::Point> > bounding_boxes;
 		std::vector<int> centre_xs;
-		std::mutex thresholded_mtx, filtered_contours_mtx, bounding_boxes_mtx, centre_xs_mtx;
+		std::mutex thresholded_mtx, filtered_contours_mtx, bounding_boxes_mtx, centre_xs_mtx, all_mtx;
+
+		//conditional varable
+		std::unique_lock<std::mutex>* u_lock;
+		std::condition_variable processed_cv;
 };
