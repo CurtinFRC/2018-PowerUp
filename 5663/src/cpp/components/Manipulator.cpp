0291012146192025eb806c9018d0571d24a6fb26
Mark Swan <mark.swan@outlook.com.au>
2018-02-03 10:38:13 +0800

    Update Manipulator class

--- 5663/src/cpp/components/Manipulator.cpp
+++ 5663/src/cpp/components/Manipulator.cpp
@@ -10,28 +10,26 @@ Manipulator::Manipulator(int intakePort, int restrainerFwd, int restrainerRev) {
 
 // Set speed of intake motor
 void Manipulator::SetIntakeSpeed(double speed) {
-  if(fabs(speed) < deadzone) {
+  if(fabs(speed) > deadzone) {
     intakeSpeed = speed;
-  }
+  } else intakeSpeed = 0;
 }
 
 // Extend actuator to close restrainer and restrain power cube
 void Manipulator::Restrain() {
-  //intakeSpeed = 0;
   restrainer->Set(restrainer->kForward);
   SmartDashboard::PutNumber("arm", restrainer->Get());
 }
 
 // Retract actuator to open restrainer and release power cube
 void Manipulator::Release() {
-  //intakeSpeed += 0.02;
   restrainer->Set(restrainer->kReverse);
   SmartDashboard::PutNumber("arm", restrainer->Get());
 }
 
 void Manipulator::RunPeriodic() {
-  if(intakeSpeed > 1) intakeSpeed = 1;
-  if(intakeSpeed < -1) intakeSpeed = -1;
+  //if(intakeSpeed > 1) intakeSpeed = 1;
+  //if(intakeSpeed < -1) intakeSpeed = -1;
   intake->Set(intakeSpeed);
   SmartDashboard::PutNumber("intakeSpeed", intakeSpeed);
 }
